88c80c349a2efb4dc65338ebc2eec46b550590e9
chapuzzo <luismax@gmail.com>
2015-07-18 17:16:55 +0200

    replaced png shells with drawn ones

--- lib/rtanque/gui/shell.rb
+++ lib/rtanque/gui/shell.rb
@@ -6,25 +6,32 @@ module RTanque
       attr_reader :shell
 
       DEBUG = ENV["DEBUG_SHELLS"]
+      WHITE  = Gosu::Color::WHITE
+      RED  = Gosu::Color::RED
 
       def initialize(window, shell)
         @window = window
         @shell = shell
         @x0 = shell.position.x
         @y0 = @window.height - shell.position.y
-        @shell_image = Gosu::Image.new(@window, Gui.resource_path("images/bullet.png"))
       end
 
       def draw
         position = [self.shell.position.x, @window.height - self.shell.position.y]
-        @shell_image.draw_rot(position[0], position[1], ZOrder::SHELL, 0, 0.5, 0.5)
 
         if DEBUG then
-          white  = Gosu::Color::WHITE
           pos = shell.position
           x1, y1 = pos.x, @window.height - pos.y
-          @window.draw_line @x0, @y0, white, x1, y1, white, ZOrder::SHELL
+          @window.draw_line @x0, @y0, WHITE, x1, y1, WHITE, ZOrder::SHELL
+          return
         end
+
+        @window.draw_quad \
+        position[0]-4, position[1]-4, RED,\
+        position[0]-4, position[1]+4, RED,\
+        position[0]+4, position[1]-4, RED,\
+        position[0]+4, position[1]+4, RED,\
+        ZOrder::SHELL
       end
     end
   end

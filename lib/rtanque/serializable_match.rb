c8e08b10e2b0adb9753c376af7ec42b12b045cd3
chapuzzo <luismax@gmail.com>
2017-11-16 18:59:49 +0100

    prevent re-including serializable modules

--- lib/rtanque/serializable_match.rb
+++ lib/rtanque/serializable_match.rb
@@ -3,10 +3,10 @@ module RTanque
 
     def initialize match
       @match = match
-      Bot.include(Serializable::Bot)
-      Shell.include(Serializable::Shell)
-      Explosion.include(Serializable::Explosion)
-      Match.include(Serializable::Match)
+      Bot.include(Serializable::Bot) unless Bot.include? Serializable::Bot
+      Shell.include(Serializable::Shell) unless Shell.include? Serializable::Shell
+      Explosion.include(Serializable::Explosion) unless Explosion.include? Serializable::Explosion
+      Match.include(Serializable::Match) unless Match.include? Serializable::Match
     end
 
     def method_missing(method, *args, &block)
